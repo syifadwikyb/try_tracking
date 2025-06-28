2759586b90f50e2fce191e0d8cbe89806130b473
Syifa Dwiky Basamala <119727680+syifadwikyb@users.noreply.github.com>
2025-06-28 20:09:54 +0700

    update

--- public/js/script.js
+++ public/js/script.js
@@ -21,13 +21,26 @@ const userIcon = L.icon({
 
 // === Rute ===
 const routeCoordinates = [
-    [-6.8054, 110.8406],
-    [-6.8080, 110.8450],
-    [-6.8103, 110.8489],
-    [-6.8142, 110.8535],
-    [-6.8190, 110.8580],
-    [-6.8235, 110.8635],
+    [-7.0538, 110.4293], // Rusunawa Undip
+    [-7.0551, 110.4297], // Masjid Hijau Sigawe
+    [-7.0561, 110.4302], // Pos Satpam Astina Undip
+    [-7.0569, 110.4310], // Student Center
+    [-7.0578, 110.4320], // Teknik Arsitektur
+    [-7.0588, 110.4332], // Fakultas Hukum & FISIP
+    [-7.0597, 110.4346], // Sekolah Vokasi
+    [-7.0602, 110.4358], // Sekolah Vokasi & FIB
+    [-7.0607, 110.4372], // Widya Puraya & Taman Inspirasi
+    [-7.0610, 110.4385], // SA-MWA & FSM Barat
+    [-7.0614, 110.4391], // Teknik Elektro
+    [-7.0608, 110.4396], // Fakultas Psikologi (Halte Trans Semarang)
+    [-7.0616, 110.4405], // Fakultas Ekonomika dan Bisnis
+    [-7.0624, 110.4413], // Fakultas Kesehatan Masyarakat
+    [-7.0632, 110.4421], // Fakultas Perikanan dan Ilmu Kelautan
+    [-7.0640, 110.4430], // Fakultas Peternakan dan Pertanian
+    [-7.0648, 110.4437], // UPT Laboratorium Terpadu
+    [-7.0655, 110.4445], // Bundaran Undip
 ];
+
 
 const routeLine = L.polyline(routeCoordinates, {
     color: "blue",
@@ -39,10 +52,10 @@ map.fitBounds(routeLine.getBounds());
 
 // === Halte ===
 const halteList = [
-    { name: "Alun-Alun Kudus", coords: [-6.8054, 110.8406] },
-    { name: "Simpang Tujuh", coords: [-6.8103, 110.8489] },
-    { name: "Pasar Kliwon", coords: [-6.8142, 110.8535] },
-    { name: "Terminal Jati", coords: [-6.8235, 110.8635] },
+    { name: "Rusunawa Undip", coords: [-7.0538, 110.4293] },
+    { name: "Masjid Hijau Sigawe", coords: [-7.0551, 110.4297] },
+    { name: "Pos Satpam Astina Undip", coords: [-7.0561, 110.4302] },
+    { name: "Halte Trans Semarang Fakultas Psikologi", coords: [-7.0608, 110.4396] },
 ];
 
 halteList.forEach((halte) => {
